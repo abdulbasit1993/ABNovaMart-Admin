84e126cf0da42664b5f1178cd9c2b94f035a4b63
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2026-01-14 17:22:31 +0500

    Updated the production API URL to use the new one

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,6 +1,7 @@
 const apiUrls = {
     local: "http://localhost:3002/api",
-    production: "https://psychological-codi-abm-apps-150283cd.koyeb.app/api"
+    production_old: "https://psychological-codi-abm-apps-150283cd.koyeb.app/api",
+    production: "https://abnovamart-backend.onrender.com/api"
 }
 
 export const BASE_URL = apiUrls.production;
